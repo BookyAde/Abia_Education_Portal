2fb0a3e3cf471c08e02aa4890483d7c6cbd51641
BookyAde <alabiwinner9@gmail.com>
2025-12-01 19:20:08 +0100

    new commit

--- app.py
+++ app.py
@@ -23,7 +23,6 @@ import csv
 from datetime import datetime
 
 
-st.write("SECRETS LOADED:", list(st.secrets.keys()))
 
 # ===================== PASSWORD HASHING FUNCTION (MUST BE AT TOP) =====================
 def hash_password(password: str) -> str:
